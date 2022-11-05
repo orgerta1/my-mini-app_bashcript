9c33c4f2659181e69f3dea6cc654db2105b7df24
andi <andi@haviari.com>
2022-11-05 17:27:23 +0100

    setup #1

---
+++ shpresa.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+USERNAMEFILE=resources/data/username.file
+
+#Chek if file exists
+##If "no", ask user for his name and store it into the given file path 
+
+#Read the USERNAMEFILE content and save it into a variable
+
+#Say Hi <username>, welcome to Shpresa
+
+#Show menu
+##Whats the date?
+##
+
+
+
+#weather info https://open-meteo.com/en/docs#latitude=41.34&longitude=19.82&daily=weathercode&timezone=Europe%2FBerlin
+
+#jq sudo apt-get install jq
+
+
+
+
